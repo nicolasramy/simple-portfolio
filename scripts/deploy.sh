cfadcb57b425b32048c18bd8071f391fb8180b37
Nicolas RAMY <nicolas.ramy@darkelda.com>
2017-06-02 01:18:31 +0200

    add collectstatic forcing

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,4 @@ git pull origin $0
 cd ..
 pip install -r requirements.txt
 python www/manage.py migrate frontend
-python www/manage.py collectstatic  --noinput
+python www/manage.py collectstatic --noinput
